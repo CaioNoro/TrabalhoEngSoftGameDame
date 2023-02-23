653e92872af4707ca1ac2fdfa57e4615df2217fe
Breno <breno.cpedroso@gmail.com>
2023-02-23 20:55:27 -0300

    Adicionado cadastro e login modificados

---
+++ gamedame/accounts/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text='Obrigatório.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Obrigatório.')
+    email = forms.EmailField(max_length=254, required=True, help_text='Obrigatório. Informe um email válido.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+        labels = {
+            'username': 'Nome de usuário',
+            'email': 'Endereço de email',
+            'first_name': 'Nome',
+            'last_name': 'Sobrenome',
+            'password1': 'Senha',
+            'password2': 'Confirmação da senha'
+        }
+
+        help_texts = {
+            'username': 'Obrigatório. 150 caracteres ou menos. Letras, dígitos e @/./+/-/_ apenas.',
+            'email': 'Obrigatório. Informe um email válido.',
+            'first_name': 'Obrigatório.',
+            'last_name': 'Obrigatório.',
+            'password1': 'Sua senha não pode ser muito parecida com suas outras informações pessoais.',
+            'password2': 'Digite a mesma senha novamente para verificação.'
+        }
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label="Nome de usuário",
+        widget=forms.TextInput(attrs={'autofocus': True, 'class': 'form-control'})
+    )
+    password = forms.CharField(
+        label="Senha",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password', 'class': 'form-control'}),
+    )
