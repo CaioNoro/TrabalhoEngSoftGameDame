4a606d9f7d8a0b3d91e9fd9220b12dcf546f61b1
Breno <breno.cpedroso@gmail.com>
2023-02-23 20:56:43 -0300

    Adicionada a tela da conta do usuário

--- gamedame/accounts/views.py
+++ gamedame/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from accounts.forms import CustomUserCreationForm, LoginForm
 
-# Create your views here.
+from games.models import PurchaseItem
+
+@login_required
+def perfilView(request):
+    games = PurchaseItem.objects.filter(
+        purchase__user=request.user).exclude(refunded=True)
+    return render(request, 'accounts/account.html', {'user': request.user, 'games': games})
