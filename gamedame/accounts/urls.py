54e02c97321da95f211c493c8b8bdb9a260b239c
Breno <breno.cpedroso@gmail.com>
2023-02-23 20:56:03 -0300

    Adicionada as urls do app accounts

---
+++ gamedame/accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import PasswordChangeView
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path('login/', views.loginUserView, name = "login"),
+    path('register/', views.registerUserView, name = 'signup'),
+    path('logout/', views.logoutUserView, name = "logout"),
+    path('profile/', views.perfilView, name = 'perfil-view'),
+    path('change-password/', PasswordChangeView.as_view(), name='change-password')
+]
+
+
+""" urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) """
