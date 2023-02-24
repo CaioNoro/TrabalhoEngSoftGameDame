26f4550deb61fe5e408ac0a5bd676723fc50b601
Breno <breno.cpedroso@gmail.com>
2023-02-23 21:02:01 -0300

    close #28, close #26, close #27: + funções ao adm

--- gamedame/games/admin.py
+++ gamedame/games/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Purchase, PurchaseItem, CartItem
+
+admin.site.register(Game)
+admin.site.register(PurchaseItem)
+admin.site.register(Purchase)
+admin.site.register(CartItem)
