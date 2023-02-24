785fb9f2d28c5293ec837aba2e8774caa670d853
Breno <breno.cpedroso@gmail.com>
2023-02-23 21:03:24 -0300

    Adicionada a criação dos models do django

--- gamedame/games/models.py
+++ gamedame/games/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    promotion = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True)
+    cover_image = models.ImageField(upload_to='static/images')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    copies_sold = models.IntegerField(default=0)
+    release_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title
+
+    def price_with_discount(self):
+        return round(self.price * (1 - self.promotion/100), 2)
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+
+class PurchaseItem(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    refunded = models.BooleanField(default=False)
+
+
+class CartItem(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.game.title} ({self.user.username})'
+    
+class Rating(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    rating = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.rating} - {self.game.title} - ({self.user.username})'
