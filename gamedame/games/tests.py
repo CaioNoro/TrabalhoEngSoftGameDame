3f5d368db08066c763bab3c0a8d06274608325aa
Breno <breno.cpedroso@gmail.com>
2023-03-03 10:49:31 -0300

    Add Casos de Teste de Validação

--- gamedame/games/tests.py
+++ gamedame/games/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    
+    def test_create_user(self):
+        user = User.objects.create_user('joao', 'joao@email.com', '123')
+        self.assertIsNotNone(user)
+        self.assertEqual(user.username, 'joao')
+        self.assertEqual(user.email, 'joao@email.com')
+        self.assertTrue(user.check_password('123'))
