80f5a595f05b15c6c45b3dcf14743d30a15daba7
Breno <breno.cpedroso@gmail.com>
2023-03-03 00:35:05 -0300

    Add visão das avaliações peki admin

--- gamedame/games/admin.py
+++ gamedame/games/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Game, Purchase, PurchaseItem, CartItem
+from .models import Game, Purchase, PurchaseItem, CartItem, Rating
 
 admin.site.register(Game)
 admin.site.register(PurchaseItem)
 admin.site.register(Purchase)
 admin.site.register(CartItem)
+admin.site.register(Rating)
